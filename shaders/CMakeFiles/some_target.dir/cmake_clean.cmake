9bf0d61adb09fee0b5062e9d3ea419d6c98e1049
Sascha Moos <sascha.moos@protonmail.com>
2020-12-16 21:00:46 +0100

    Gitignore files removed

--- shaders/CMakeFiles/some_target.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/usr/share/cmake-3.19/build/*.spv"
-  "CMakeFiles/some_target"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/some_target.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
